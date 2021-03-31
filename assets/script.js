cc7de4987cd9beb31d2062d851203a9004caab47
Jackson Peak <jacksonpeak1@icloud.com>
2021-03-30 23:43:06 -0400

    added https to links in script.js file

--- assets/script.js
+++ assets/script.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
   //get the weather current data
   function getCurrentWeather(cityName) {
     var settings = {
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`,
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`,
       method: "GET",
       timeout: 0,
     };
@@ -59,7 +59,7 @@ $(document).ready(function () {
           <div class="card-body" id="current-weather-body">
             <h3 class="card-title">
               ${response.name} (${new Date().toLocaleDateString()})
-              <img src="http://openweathermap.org/img/w/${
+              <img src="https://openweathermap.org/img/w/${
                 response.weather[0].icon
               }.png">
             </h3>
@@ -105,7 +105,7 @@ $(document).ready(function () {
           forecastCard.innerHTML = `<h3>${forecastDoW}</h3>
           <p>Temperature: ${response.daily[i].temp.day} °F</p>
           <p>Humidity: ${response.daily[i].humidity} %</p>
-          <img src="http://openweathermap.org/img/wn/${response.daily[i].weather[0].icon}.png">`;
+          <img src="https://openweathermap.org/img/wn/${response.daily[i].weather[0].icon}.png">`;
 
           forecastDiv.append(forecastCard);
         }
@@ -114,84 +114,3 @@ $(document).ready(function () {
   }
 });
 
-// var searchForm = $("#search-form");
-// var searchTermEl = $("#search-term");
-// var imagesDisplay = $("#images-display");
-
-// searchForm.on("submit", function (event) {
-//   event.preventDefault();
-//   // Take the search term from out of the input.
-//   var searchTerm- searchTermEl.val();
-//   console.log(searchTerm);
-//   var apiKey = "10a522f9d8216f7c5d46d3a3132e73b0";
-//   // Build the api url with search term and the api key
-//   // store teh api key in a variable
-//   var queryURL = "http://openweathermap.org/img/w/search?api_key=" + apiKey + "&q" + searchTerm + "&limit=3";
-
-//   // Make an API call using fetch
-//   fetch(queryURL)
-//   .then(function response) {
-//     return response.json[];
-//   })
-//   .then (function (data) {
-//     console.log(data.data[0].images.fixed_width_small.url);
-
-//     imagesDisplay.empty();
-
-//     for (var i = 0; i < data.data.length; i++) {
-//       var imageEl= $("<img>");
-//       imageEl.addclass("col-sm-4");
-//       imageEl.attr("src", data.data[i].images.fixed_width_small.url)
-//       imagesDisplay.append(imageEl);
-//     }
-//   })
-
-// }
-
-// $.getJSON(
-//   "https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=10a522f9d8216f7c5d46d3a3132e73b0",
-//   function (data) {
-//     console.log(data);
-
-//     var icon =
-//       "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-//     var temp = Math.floor(data.main.temp);
-//     var weather = data.weather[0].main;
-
-//     $(".icon").attr("src", icon);
-//     $(".weather").append(weather);
-//     $(".temp").append(temp);
-//   }
-// );
-
-// fetch(
-//   "https://api.openweathermap.org/data/2.5/weather?q=Atlanta&units=imperial&appid=10a522f9d8216f7c5d46d3a3132e73b0"
-// )
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   });
-
-/* <script>
-function myFunction() {
-  // Declare variables
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("mySearch");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myMenu");
-  li = ul.getElementsByTagName("li");
-
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-} */
-
-// "https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=10a522f9d8216f7c5d46d3a3132e73b0"
